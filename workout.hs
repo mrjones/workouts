a8f09bb9c075e819a4648e412edb63742bc4e9c1
Matt Jones <jonesmr+github@gmail.com>
2014-12-07 23:17:24 -0500

    print a single string from the database to the web page

--- workout.hs
+++ workout.hs
@@ -1,14 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad (liftM)
+import Control.Monad.IO.Class (liftIO)
+--import qualified Data.Text.Lazy as T (Text)
+-- sudo apt-get install libmysqlclient-dev
+-- cabal install mysql-simple
+import Database.MySQL.Simple
+import Database.MySQL.Simple.Types as SQLT
 -- cabal install happstack
-import Happstack.Lite
-import Data.Text (Text)
+import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, ServerPart, Response)
+import Text.Blaze.Html5 as H
+import Text.Blaze.Html5.Attributes as A
+
+main:: IO()
+main = do
+  simpleHTTP nullConf $ helloPage
+
+helloPage :: ServerPart Response
+helloPage = do
+    message <- liftIO $ myHead getMessage
+    ok $ toResponse $ pageWithMessage message
+
+pageWithMessage :: String -> H.Html
+pageWithMessage msg =
+  H.html $ do
+    H.head $ do
+      H.title $ "Hello, world!"
+    H.body $ do
+      H.toHtml msg
+
+myHead :: IO [String] -> IO String
+myHead xs = liftM Prelude.head xs
+
+getMessage :: IO [String]
+getMessage = do
+  conn <- connect defaultConnectInfo
+    { connectUser = "happstack"
+    , connectPassword = "happstack"
+    , connectDatabase = "happstack"
+    }
+  results <- query conn "SELECT Message FROM foo" ()
+  return $ Prelude.map(\(Only r) -> r) results
+                                                        
+
+{-|
 import Text.Blaze.Html5 (Html, toHtml)
 import qualified Text.Blaze.Html5 as H
- 
-main:: IO()
-main = serve Nothing workoutApp
-
-workoutApp :: ServerPart Response
-workoutApp = msum [ homePage ]
+import Data.Text (Text)
 
 homePage :: ServerPart Response
 homePage = ok $ template "home page" $ do H.h1 (toHtml "Hello!")
@@ -20,3 +58,4 @@ template title body = toResponse $
                           H.title (toHtml title)
                         H.body $ do
                           body
+-}
