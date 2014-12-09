8323fda3abbb8441325dd228d912a87150a5ffef
Matt Jones <jonesmr+github@gmail.com>
2014-12-08 21:28:51 -0500

    CREATE TABLE sql

--- workout.hs
+++ workout.hs
@@ -1,25 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Control.Monad (liftM)
+import Control.Monad (liftM, msum)
 import Control.Monad.IO.Class (liftIO)
---import qualified Data.Text.Lazy as T (Text)
+import Data.Int (Int64)
 -- sudo apt-get install libmysqlclient-dev
 -- cabal install mysql-simple
 import Database.MySQL.Simple
 import Database.MySQL.Simple.Types as SQLT
 -- cabal install happstack
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, ServerPart, Response)
+import Happstack.Server (dir, nullConf, simpleHTTP, toResponse, ok, ServerPart, Response, ServerPartT)
 import Text.Blaze.Html5 as H
-import Text.Blaze.Html5.Attributes as A
+--import Text.Blaze.Html5.Attributes as A
 
 main:: IO()
 main = do
-  simpleHTTP nullConf $ helloPage
+  simpleHTTP nullConf $ allPages
 
-helloPage :: ServerPart Response
+allPages :: ServerPartT IO Response
+allPages = msum
+           [ mkTablePage
+           , helloPage
+           ]
+
+mkTablePage :: ServerPartT IO Response
+mkTablePage = dir "admin" $ dir "mktable" $ do
+  i <- liftIO mkTable
+  ok (toResponse (mkTableHtml i))
+
+mkTableHtml :: Int64 -> H.Html
+mkTableHtml _ =
+  H.html $ do
+    H.head $ do
+      H.title $ "mktable page"
+    H.body "mktable"
+
+helloPage :: ServerPartT IO Response
 helloPage = do
-    message <- liftIO $ myHead getMessage
-    ok $ toResponse $ pageWithMessage message
+    message <- liftIoMyHead
+    helloPage2 message
+
+helloPage2 :: String -> ServerPartT IO Response
+helloPage2 msg = ok (toResponse (pageWithMessage msg))
 
 pageWithMessage :: String -> H.Html
 pageWithMessage msg =
@@ -29,33 +50,26 @@ pageWithMessage msg =
     H.body $ do
       H.toHtml msg
 
+liftIoMyHead :: ServerPartT IO String
+liftIoMyHead = liftIO $ myHead getMessage
+
 myHead :: IO [String] -> IO String
 myHead xs = liftM Prelude.head xs
 
-getMessage :: IO [String]
-getMessage = do
-  conn <- connect defaultConnectInfo
+dbConnect = connect defaultConnectInfo
     { connectUser = "happstack"
     , connectPassword = "happstack"
     , connectDatabase = "happstack"
     }
+
+getMessage :: IO [String]
+getMessage = do
+  conn <- dbConnect
   results <- query conn "SELECT Message FROM foo" ()
   return $ Prelude.map(\(Only r) -> r) results
                                                         
 
-{-|
-import Text.Blaze.Html5 (Html, toHtml)
-import qualified Text.Blaze.Html5 as H
-import Data.Text (Text)
-
-homePage :: ServerPart Response
-homePage = ok $ template "home page" $ do H.h1 (toHtml "Hello!")
-
-template :: [Char] -> Html -> Response
-template title body = toResponse $
-                      H.html $ do
-                        H.head $ do
-                          H.title (toHtml title)
-                        H.body $ do
-                          body
--}
+mkTable :: IO Int64
+mkTable = do
+  conn <- dbConnect
+  execute conn "CREATE TABLE happstack.runs (millimiles INT, duration_sec INT, comment VARCHAR(255))" ()
