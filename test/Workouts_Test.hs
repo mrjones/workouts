5d38b75b49a90e5cc9c56223a2fe79f24e68163c
Matt Jones <jonesmr@gmail.com>
2014-12-28 19:07:15 -0500

    first real test

--- test/Workouts_Test.hs
+++ test/Workouts_Test.hs
@@ -14,9 +14,9 @@ workoutsSuite =
 
 testComputeRest :: Assertion
 testComputeRest =
-  assertEqual "rest" [0, 1, 1, 10]
-  (W.computeRest [ (fromGregorian 2000 1 1)
-                 , (fromGregorian 2000 1 2)
-                 , (fromGregorian 2000 1 3)
-                 , (fromGregorian 2000 1 13)
-                 ])
+  assertEqual "rest" [0, 1, 1, 10] $
+  W.computeRest [ (fromGregorian 2000 1 1)
+                , (fromGregorian 2000 1 2)
+                , (fromGregorian 2000 1 3)
+                , (fromGregorian 2000 1 13)
+                ]
